7a5a9c94f2e09dabb4c8d0f05d4574f73a012995
Kim Keiser <Kimalexanderkeiser@gmail.com>
2023-05-09 20:38:01 +0200

    Refactored scenes, Pause Menu is functional.

--- Unity FootBall Game/Assets/Scripts/MainMenu.cs
+++ Unity FootBall Game/Assets/Scripts/MainMenu.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -9,6 +11,21 @@ public class MainMenu : MonoBehaviour
     {
         // Gets the current active scene +1 to index number; starts at 0 MainMenu
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+        Debug.Log("Level2 loaded");
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+        Debug.Log("Level3 loaded");
     }
 
     public void QuitGame()
---
+++ Unity FootBall Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    // Ref to game obj
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // Resume time
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // Stops time
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+}
